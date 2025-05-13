fc5ebe4e150811458677e171991f3036b06ddfb5
Sergey Josue Soto Sucre <ssoto-su@c2r6s6.42malaga.com>
2025-05-13 21:49:43 +0200

    todas las funciones

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ssoto-su <ssoto-su@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 18:39:46 by ssoto-su          #+#    #+#             */
-/*   Updated: 2025/05/12 12:56:51 by ssoto-su         ###   ########.fr       */
+/*   Updated: 2025/05/13 20:04:40 by ssoto-su         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 #endif
