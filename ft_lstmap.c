e7115cfd6575313f8864082ec0adc151db7a71a4
Sergey Josue Soto Sucre <ssoto-su@c2r3s6.42malaga.com>
2025-05-19 17:35:50 +0200

    Funciones bonus terminadas

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,39 +6,30 @@
 /*   By: ssoto-su <ssoto-su@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 14:58:13 by ssoto-su          #+#    #+#             */
-/*   Updated: 2025/05/16 20:46:35 by ssoto-su         ###   ########.fr       */
+/*   Updated: 2025/05/19 16:36:32 by ssoto-su         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*inicio_new_list;
+	t_list	*new_list;
 	t_list	*temp;
 
 	if (!lst || !f || !del)
-		return NULL;
-	while(lst)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
 	{
-		if(!inicio_new_list)
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
 		{
-			inicio_new_list = ft_lstnew(f(lst->content));
-			if (!inicio_new_list)
-				return (NULL);
+			ft_lstclear(&new_list, del);
+			return (NULL);
 		}
-		else 
-		{
-			temp = ft_lstnew(f(lst->content));
-			if (!temp)
-			{
-				ft_lstclear(&temp, del);
-				return (NULL);
-			}
-			ft_lstadd_back(&inicio_new_list, temp);
-		}
+		ft_lstadd_back(&new_list, temp);
 		lst = lst->next;
 	}
-	temp->next = NULL;
-	return (inicio_new_list);
+	return (new_list);
 }
